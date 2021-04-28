f3db8b4be843873eddb209fb739aa910d1b8cc72
CCKIDD <77047385+CCKIDD@users.noreply.github.com>
2021-04-28 17:44:07 -0400

    Added date to header

--- client/src/components/TopAppBar.jsx
+++ client/src/components/TopAppBar.jsx
@@ -6,6 +6,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 //import BudgetMonthNav from './BudgetMonthNav';
+import BasicDatePicker from './BasicDatePicker'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,11 +27,12 @@ export default function TopAppBar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            BudgetFree
-          </Typography>
+          
           {/* <BudgetMonthNav/> */}
           <SelectBudget  budgetId={props.budgetId} setBudgetId={props.setBudgetId} />
+          <Typography variant="h6" className={classes.title}>
+          <BasicDatePicker/>
+          </Typography>
           
           <LogoutButton />
         </Toolbar>
