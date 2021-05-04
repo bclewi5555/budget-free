69376de9471005c372a581f81679a035274cb7b5
Brandon Lewis <31356859+bclewi5555@users.noreply.github.com>
2021-05-04 13:56:06 -0400

    decouple express api from React client

--- client/src/components/detailListComponents/FundDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function FundDetail() {
-  return(
-    <div>
-      {/* Detailed properties and calculations of the fund */}
-    </div>
-  );
-}
--- client/src/services/UserService.js
+++
@@ -1,8 +0,0 @@
-//import axios from 'axios';
-
-exports.getUser = async () => {
-  //const res = await fetch('/api/v1/users');
-  const res = await axios.get('/api/v1/users');
-  console.log(res.data);
-  return res;
-}
